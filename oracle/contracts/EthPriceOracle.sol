3b288303ada5d4cc2a7e9a43352e8be12a3e23ce
Milissa Chau <mchau@student.21-school.ru>
2020-12-20 03:05:17 +0300

    oracle is the biggest disappointment in life

--- oracle/contracts/EthPriceOracle.sol
+++ oracle/contracts/EthPriceOracle.sol
@@ -9,6 +9,19 @@ contract EthPriceOracle is Ownable {
   event SetLatestEthPriceEvent(uint256 ethPrice, address callerAddress);
   
   function getLatestEthPrice() public returns(uint256) {
-	  randNonce++;
+	randNonce++;
+	uint id = uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % modulus;
+	pendingRequests[id] = true;
+	emit GetLatestEthPriceEvent(msg.sender, id);
+	return id;
+  }
+  
+  function setLatestEthPrice(uint256 _ethPrice, address _callerAddress, uint256 _id) public onlyOwner {
+	require(pendingRequests[_id], "This request is not in my pending list.");
+	delete pendingRequests[_id];
+	CallerContracInterface callerContractInstance;
+    callerContractInstance = CallerContracInterface(_callerAddress);
+	callerContractInstance.callback(_ethPrice, _id);
+	emit SetLatestEthPriceEvent(_ethPrice, _callerAddress);
   }
 }
