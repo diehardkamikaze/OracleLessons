1a771d5ae76a9205a30fe18841fdf0e34c07ab72
Milissa Chau <mchau@student.21-school.ru>
2020-12-21 02:25:44 +0300

    Start of making backend

--- oracle/contracts/CallerContractInterface.sol
+++ oracle/contracts/CallerContractInterface.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.5.0;
+pragma solidity >=0.4.22 <0.9.0;
 contract CallerContractInterface {
   function callback(uint256 _ethPrice, uint256 id) public;
 }
--- oracle/contracts/EthPriceOracle.sol
+++ oracle/contracts/EthPriceOracle.sol
@@ -1,5 +1,5 @@
-pragma solidity 0.5.0;
-import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+pragma solidity >=0.4.22 <0.9.0;
+import "../../openzeppelin-solidity/contracts/access/Ownable.sol";
 import "./CallerContractInterface.sol";
 contract EthPriceOracle is Ownable {
   uint private randNonce = 0;
@@ -19,8 +19,8 @@ contract EthPriceOracle is Ownable {
   function setLatestEthPrice(uint256 _ethPrice, address _callerAddress, uint256 _id) public onlyOwner {
 	require(pendingRequests[_id], "This request is not in my pending list.");
 	delete pendingRequests[_id];
-	CallerContracInterface callerContractInstance;
-    callerContractInstance = CallerContracInterface(_callerAddress);
+	CallerContractInterface callerContractInstance;
+    callerContractInstance = CallerContractInterface(_callerAddress);
 	callerContractInstance.callback(_ethPrice, _id);
 	emit SetLatestEthPriceEvent(_ethPrice, _callerAddress);
   }
